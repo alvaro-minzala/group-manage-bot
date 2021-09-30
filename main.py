b57f9c37c2f8c3e5635a2dcdd9c36c3130a9f03e
alvaro-minzala <71344754+alvaro-minzala@users.noreply.github.com>
2021-09-30 13:36:57 +0000

    Comments about every action

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import discord
 import os
-
+#discord (up) client loader(down)
 client = discord.Client()
 
-
+#messages
 @client.event
 async def on_ready():
     print("Hello, I'm {0.user}. Hello World ")
@@ -14,5 +14,5 @@ async def onmessage(message):
     if message.author == client.user:
         return
 
-
+#Token environment variable link
 my_secret = os.environ['token']
